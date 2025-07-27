9c3b961cea25aaa2ee02591ee60ac570e5587b1d
gunjou <gjoefandi@gmail.com>
2025-07-27 15:44:05 +0800

    tambah penyesuaian route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Login from "./login";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="">Home</div>
+    </div>
+  );
+};
+
+export default Home;
